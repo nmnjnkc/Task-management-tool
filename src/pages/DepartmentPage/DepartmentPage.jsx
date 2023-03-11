6862370f752bdec87017504af0ced17b996a1082
nmnjnkc <nmnjnkc@yahoo.com>
2023-03-11 23:42:27 +0100

    add error page

--- src/pages/DepartmentPage/DepartmentPage.jsx
+++ src/pages/DepartmentPage/DepartmentPage.jsx
@@ -1,17 +1,44 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import { useParams } from 'react-router';
 import ApplicationContext from '../../ApplicationContext';
+import SearchError from '../../components/SearchError/SearchError';
 
 const DepartmentPage = () => {
 
   const {departments, activeDep} = useContext(ApplicationContext);
+  const [showError, setShowError] = useState(false);
+
 
   let { departmentId } = useParams();
 
-  const currentDep = departments.find((department) => department.id == departmentId)
+  const allDepartmentsIds = departments?.map(dep => dep.id)
+
+  const currentDep = departments?.find((department) => department.id == departmentId)
+
+  const noDep = allDepartmentsIds?.some(id => id == departmentId)
+
+  useEffect(() => {
+    let timeoutId;
+    if (noDep === false) {
+      timeoutId = setTimeout(() => {
+        setShowError(true);
+      }, 250); 
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [noDep]);
+
+  console.log(noDep);
 
   return (
-    <div>{currentDep?.department}</div>
+    <div>
+      <h3>{currentDep?.department}</h3>
+
+      {showError && <SearchError message={"There's no such Department."}/>}
+      
+    </div>
   )
 }
 
--- src/pages/EmployeePage/EmployeePage.jsx
+++ src/pages/EmployeePage/EmployeePage.jsx
@@ -1,25 +1,41 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom';
 import ApplicationContext from '../../ApplicationContext';
+import SearchError from '../../components/SearchError/SearchError';
 
 
 const EmployeePage = () => {
 
-
   const {employees, activeEmployee} = useContext(ApplicationContext);
+  const [showError, setShowError] = useState(false);
+
   let { employeeId } = useParams();
+
   const currentEmp = employees.find((employee) => employee.id == employeeId);
+  const allEmployees = employees?.map(emp => emp.id)
+  const noEmp = allEmployees?.some(id => id == employeeId)
 
+  useEffect(() => {
+    let timeoutId;
+    if (noEmp === false) {
+      timeoutId = setTimeout(() => {
+        setShowError(true);
+      }, 250); 
+    }
 
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [noEmp]);
 
-console.log(activeEmployee);
+  console.log(noEmp);
 
   return (
     <div>
       
         <h3>{currentEmp?.fullName}</h3>
 
-    
+        {showError && <SearchError message={"There's no such Employee."}/>}
 
     </div>
   )
